f42fd6a57ff6cff913c874ee457997ed2238ef53
Taylor Johnson <johnsoctbus@gmail.com>
2024-10-18 14:17:32 -0400

    completed

--- index.js
+++ index.js
@@ -1,20 +1,35 @@
 var express = require('express');
 var cors = require('cors');
 require('dotenv').config()
+const multer = require('multer')
 
 var app = express();
+const upload = multer({ dest: 'uploads/' })
 
 app.use(cors());
 app.use('/public', express.static(process.cwd() + '/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+        res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse', upload.single('upfile'), function (req, res, next) {
+        try {
+                console.log(req.file)
+                res.json({ 
+                        file: req.file,
+                        name: req.file.originalname,
+                        type: req.file.mimetype,
+                        size: req.file.size,
+                })
+        }
+        catch (error) {
+                next(error)
+        }
+})
 
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
-  console.log('Your app is listening on port ' + port)
+        console.log('Your app is listening on port ' + port)
 });
